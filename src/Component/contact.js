ece7eb5e5a797335af5a1db10d283d7a4ec32ecf
Edeh sharon <Pearllizzy196@gmail.com>
2022-11-03 18:49:01 +0100

    styled the background

--- src/Component/contact.js
+++ src/Component/contact.js
@@ -16,30 +16,38 @@ const ContactForm = () => {
     }
     return(
             <div className="container mt-5">
-      <h2 className="mb-3">React Contact Form Component Example</h2>
+      <h1 className="mb-3">Contact Me</h1>
+      <h4><small>Hi there contact me to ask me about anything you have in mind.</small></h4>
       <form onSubmit={onSubmit}>
         <div className="mb-3">
           <label className="form-label" htmlFor="firstName">
-            FirstName
+            First name
           </label>
-          <input className="form-control" type="text" id="first-name" required />
+          <input className="form-control" type="text" id="first_name" placeholder="enter your first name" required />
           <label className="form-label" htmlFor="lastName">
-            lastName
+            Last name
           </label>
-          <input className="form-control" type="text" id="last-name" required />
+          <input className="form-control" type="text" id="last_name" placeholder="enter your last name" required />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="email">
             Email
           </label>
-          <input className="form-control" type="email" id="email" required />
+          <input className="form-control" type="email" id="email" placeholder="yourname@email.com" required />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="message">
             Message
           </label>
-          <textarea className="form-control" id="message" required />
+          <textarea className="form-control" id="message" placeholder="send me a message i will reply as soon as possible" required />
         </div>
+        <div>
+            <label>
+                <input type="checkbox" required/> 
+                you agree to providing your data to Edeh Sharon who may contact you
+            </label>
+        </div>
+        <p></p>
         <button className="btn__submit" type="submit">
           {formStatus}
         </button>
