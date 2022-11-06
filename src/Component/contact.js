68c06bc4ffd16076a46936ad5ffd3588d75aacc7
Edeh sharon <110258878+shannychi@users.noreply.github.com>
2022-11-06 15:50:34 +0100

    Update contact.js

--- src/Component/contact.js
+++ src/Component/contact.js
@@ -52,8 +52,10 @@ const ContactForm = () => {
             </label>
         </div>
         <p></p>
+          <div class=" d-grip gap-2 d-md-flex justify-content-md-end">
         <button class="btn btn-primary" id="btn__submit" type="submit" >
-          {formStatus}
+            </div>
+     
         </button>
       </form>
         </div>
